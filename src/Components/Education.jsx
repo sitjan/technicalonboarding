71597ec3a0d1682c1182097ee01e8e2e3ea6e3a2
Janak <99483099+sitjan@users.noreply.github.com>
2024-02-27 11:42:12 +0000

    education

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -4,6 +4,7 @@ const Education = () => {
     return(
         <section className="light" id="education">
             <h2>Education</h2>
+            <h3> BE in Computer Engineering at Kathmandu University (2021 - Present)</h3>
         </section>
     )
 }
